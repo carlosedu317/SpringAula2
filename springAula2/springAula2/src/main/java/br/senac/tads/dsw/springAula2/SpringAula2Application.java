a01f539f351fb5fde5c4b6015fbceb3f64c9cedc
Carlos Eduardo <141144946+carlosedu317@users.noreply.github.com>
2026-08-25 10:11:22 -0300

    Add files via upload

---
+++ springAula2/springAula2/src/main/java/br/senac/tads/dsw/springAula2/SpringAula2Application.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.springAula2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAula2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAula2Application.class, args);
+	}
+
+}
---
+++ springAula2/springAula2/src/test/java/br/senac/tads/dsw/springAula2/SpringAula2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.springAula2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAula2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
